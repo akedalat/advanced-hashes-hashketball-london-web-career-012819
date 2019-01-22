29be1b9c3a687786a04b9beb27a27d1ce59b2df2
Kian Edalat <akedalat@gmail.com>
2019-01-22 13:27:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ require "pry"
 def game_hash
 game_hash = {
 :home => {
-    :team_name => "Brooklyn Nets",
+    :team_name => ["Brooklyn Nets"],
     :colors => ["Black", "White"],
     :players => {
           "Alan Anderson" => {
